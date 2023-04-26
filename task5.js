692f0780cdc9b39b7bcd6754b2c6286aa35e620a
SKORNEV74 <yasher7495@yandex.ru>
2023-04-27 01:54:10 +0300

    upload 3

--- task5.js
+++ task5.js
@@ -1,9 +1,27 @@
 const btn = document.querySelector('.btn');
 const outer = document.querySelector('.outer');
 
-btn.addEventListener('click', () => {
-    const page = document.querySelector('#page');
-    const limit = document.querySelector('#limit');
+const useRequest = () => {
+    const page = +document.querySelector('#page').value;
+    const limit = +document.querySelector('#limit').value;
 
-    if (page)
-})
+    console.log(`${page}, ${limit}`);
+
+    if ((page < 1 || page > 10) && (limit < 1 || limit > 10)) {
+        outer.innerHTML = "Номер страницы вне диапазона от 1 до 10";
+
+    } else if (limit < 1 || limit > 10) {
+        outer.innerHTML = "Лимит вне диапазона от 1 до 10";
+
+    } else if (page < 1 || page > 10) {
+        outer.innerHTML = "Номер страницы и лимит вне диапазона от 1 до 10";
+
+    } else {
+        outer.innerHTML = "Vse chiki-puki!";
+    }
+}
+
+
+btn.addEventListener('click', async () => {
+    await useRequest();
+});
